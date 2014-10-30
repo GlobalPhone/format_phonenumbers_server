4c3e90b541cf597930aee8896f282f9e561a5e69
Oskar Gewalli <oskar@gewalli.se>
2014-10-30 20:33:42 +0100

    less cruft

--- routes/format.js
+++ routes/format.js
@@ -2,37 +2,32 @@ var libphonenumber = require('libphonenumber');
 var express = require('express');
 var router = express.Router();
 
-router.all('/e164/:country_code/:number', function(req, res) {
-    console.log('getting a request');
-    var number = req.params.number;
-    libphonenumber.e164(number, req.params.country_code, function (error, result) {
+function return_result(number,res){
+    return function(error, result){
         if (error){
             res.status(403);
             res.send(error.message);
         }else{
             res.json({
                 number:number,
-                e164:result
+                result:result
             });
         }
-    });
+    }
+}
+
+router.all('/e164/:country_code/:number', function(req, res) {
+    var number = req.params.number;
+    libphonenumber.e164(number, 
+        req.params.country_code, 
+        return_result(number, res));
 });
 
 router.all('/intl/:country_code/:number', function(req, res) {
     var number = req.params.number;
-    libphonenumber.intl(number, req.params.country_code, function (error, result) {
-        if (error){
-            res.status(403);
-            res.send(error.message);
-        }else{
-            res.json({
-                number:number,
-                intl:result
-            });
-        }
-
-    });
+    libphonenumber.intl(number, 
+        req.params.country_code, 
+        return_result(number, res));
 });
-
 
 module.exports = router;
