2ce160e5efcd6b1ba26568c7a02bac317da1d3c7
Oskar Gewalli <oskar@gewalli.se>
2014-11-01 06:01:23 +0100

    No need for users

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 var format = require('./routes/format');
 var formatBulk = require('./routes/format_bulk');
 
@@ -25,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 app.use('/format', format);
 app.use('/format_bulk',formatBulk);
 
