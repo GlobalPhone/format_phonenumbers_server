89b922447f722a8a4f304ae33d0f655b93608a79
Oskar Gewalli <oskar@gewalli.se>
2014-11-01 05:57:55 +0100

    Daniel - this is how you write that simpler ;)

--- routes/format_bulk.js
+++ routes/format_bulk.js
@@ -1,70 +1,40 @@
-var libphonenumber = require('libphonenumber');
-var Q = require('q');
 var express = require('express');
+var promise = require('bluebird');
+var libphonenumber = promise.promisifyAll(require('libphonenumber'));
 var router = express.Router();
-
-function returnWithResult(number, deferred){
-    return function(error, result){
-        if (error){
-            deferred.resolve({
-                number:number,
-                message:error.message
-            });
-        }else{
-            deferred.resolve({
-                number:number,
-                result:result
-            });
-        }
+ 
+router.get('/e164/:country_code/:numbers', function(req, res, next) {
+    var numbers = req.params.numbers.split(',');
+    var country_code = req.params.country_code;
+    process('e164', numbers, country_code, req, res, next);
+});
+ 
+router.get('/intl/:country_code/:numbers', function(req, res, next) {
+    var numbers = req.params.numbers.split(',');
+    var country_code = req.params.country_code;
+    process('intl', numbers, country_code, req, res, next);
+});
+ 
+router.post('/e164/:country_code', function(req, res, next) {
+    var numbers = req.body.numbers.split(',');
+    var country_code = req.params.country_code;
+    process('e164', numbers, country_code, req, res, next);
+});
+ 
+router.post('/intl/:country_code', function(req, res, next) {
+    var numbers = req.body.numbers.split(',');
+    var country_code = req.params.country_code;
+    process('intl', numbers, country_code, req, res, next);
+});
+ 
+function process(formatter, numbers, country_code, req, res, next) {
+    function format(n) {
+        return libphonenumber[formatter + 'Async'](n, country_code)
+            .then(function (result) { return { number: n, result: result } })
+            .catch(function (err) { res.status(422); return { number: n, message: err.message } });
     }
+ 
+    return promise.all(numbers.map(format)).then(function (n) { res.jsonp(n); }).catch(next);
 }
-
-function forAllNumbersWithCountryCode(numbers, country_code, format){
-    var array = [];
-    numbers.forEach(function(number){
-        var deferred = Q.defer();
-        format(number, 
-            country_code, 
-            returnWithResult(number, deferred));
-        array.push(deferred.promise);
-    });
-    return Q.all(array);
-}
-
-function hasAnError(results){
-    return results.some(function(result){ return 'message' in result; });
-}
-
-function respondWithJson(res){
-    return function(results){
-        if (hasAnError(results)){
-            res.status(422);
-        }
-        res.json(results);
-    };
-}
-router.get('/e164/:country_code/:numbers', function(req, res) {
-    var numbers = req.params.numbers.split(',');
-    var country_code = req.params.country_code;
-    forAllNumbersWithCountryCode(numbers, country_code, libphonenumber.e164).then(respondWithJson(res));
-});
-
-router.get('/intl/:country_code/:numbers', function(req, res) {
-    var numbers = req.params.numbers.split(',');
-    var country_code = req.params.country_code;
-    forAllNumbersWithCountryCode(numbers, country_code, libphonenumber.intl).then(respondWithJson(res));
-});
-
-router.post('/e164/:country_code', function(req, res) {
-    var numbers = req.body.numbers.split(',');
-    var country_code = req.params.country_code;
-    forAllNumbersWithCountryCode(numbers, country_code, libphonenumber.e164).then(respondWithJson(res));
-});
-
-router.post('/intl/:country_code', function(req, res) {
-    var numbers = req.body.numbers.split(',');
-    var country_code = req.params.country_code;
-    forAllNumbersWithCountryCode(numbers, country_code, libphonenumber.intl).then(respondWithJson(res));
-});
-
+ 
 module.exports = router;
