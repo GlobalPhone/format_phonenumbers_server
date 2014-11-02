1b8c9c176caac941cc2dda7e64fee211faf0b1f5
Oskar Gewalli <oskar@gewalli.se>
2014-11-02 07:51:41 +0100

    Test that it can handle multiple numbers

--- test/format_bulk.js
+++ test/format_bulk.js
@@ -3,7 +3,6 @@ var assert = require('assert');
 var request = require('supertest');  
 var app = require('../app');
 
-var url = 'http://localhost:3001';
 describe('Format Bulk', function() {
   var server;
   before(function () {
@@ -12,19 +11,21 @@ describe('Format Bulk', function() {
   after(function () {
     server.close();
   });
+  function requestFormat(formatter, country_code, numbers){
+    var url = 'http://localhost:3001';
+    return request(url)
+        .post('/format_bulk/'+formatter+'/'+country_code)
+        .send({numbers:numbers})
+        .expect('Content-Type', /json/);
+  }
 
   describe('e164',function(){
     it('should return error trying get invalid number', function(done) {
       var country_code = 'SE';
       var number = '124311';
-      request(url)
-        .post('/format_bulk/e164/'+country_code)
-        .send({numbers:number})
-        .expect('Content-Type', /json/)
+      requestFormat('e164', country_code, number)
         .end(function(err, res) {
-          if (err) {
-            throw err;
-          }
+          should.not.exist(err);
           res.status.should.equal(422);
           done();
         });
@@ -32,33 +33,37 @@ describe('Format Bulk', function() {
     it('should return formatted number when valid', function(done) {
       var country_code = 'SE';
       var number = '0767201010';
-      request(url)
-        .post('/format_bulk/e164/'+country_code)
-        .send({numbers:number})
-        .expect('Content-Type', /json/)
-        .expect(200) //Status code
+      requestFormat('e164', country_code, number)
         .end(function(err, res) {
-          if (err) {
-            throw err;
-          }
+          should.not.exist(err);
+          res.status.should.equal(200);
           res.body[0].result.should.equal('+46767201010');
           done();
         });
     });
+    it('should handle multiple numbers', function(done) {
+      var country_code = 'SE';
+      var numbers = '0767201010,0767201011,0767201012,0767201013,0767201014';
+      requestFormat('e164', country_code, numbers)
+        .end(function(err, res) {
+          should.not.exist(err);
+          res.status.should.equal(200);
+          res.body.length.should.equal(5);
+          res.body[0].result.should.equal('+46767201010');
+          res.body[4].result.should.equal('+46767201014');
+          done();
+        });
+    });
+
   });
 
   describe('intl',function(){
     it('should return error trying get invalid number', function(done) {
       var country_code = 'SE';
       var number = '124311';
-      request(url)
-        .post('/format_bulk/intl/'+country_code)
-        .send({numbers:number})
-        .expect('Content-Type', /json/)
+      requestFormat('intl', country_code, number)
         .end(function(err, res) {
-          if (err) {
-            throw err;
-          }
+          should.not.exist(err);
           res.status.should.equal(422);
           done();
         });
@@ -66,15 +71,10 @@ describe('Format Bulk', function() {
     it('should return formatted number when valid', function(done) {
       var country_code = 'SE';
       var number = '0767201010';
-      request(url)
-        .post('/format_bulk/intl/'+country_code)
-        .send({numbers:number})
-        .expect('Content-Type', /json/)
-        .expect(200) //Status code
+      requestFormat('intl', country_code, number)
         .end(function(err, res) {
-          if (err) {
-            throw err;
-          }
+          should.not.exist(err);
+          res.status.should.equal(200);
           res.body[0].result.should.equal('+46 76 720 10 10');
           done();
         });
